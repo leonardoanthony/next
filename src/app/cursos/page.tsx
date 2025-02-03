2343d5a6def94a82ea56b22a8288ceae8633898c
leonardoanthony <leonardoanthony.dev@gmail.com>
2025-02-03 17:43:28 -0300

    :wrench: fix: Renderização

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,6 +1,9 @@
 import { getCursos } from '@/api/cursos';
 import Link from 'next/link';
 
+export const dynamic = 'force-dynamic';
+// export const revalidate = 0;
+
 export default async function CursosPage() {
   const cursos = await getCursos();
   return (
