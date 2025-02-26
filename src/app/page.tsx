1b75212462287f5a187e7d8175972fbf24a3283e
leonardoanthony <leonardoanthony.dev@gmail.com>
2025-02-26 15:36:07 -0300

    :sparkles: feat: Server Actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+
 export default function HomePage() {
+
+  function handleClick(){
+    console.log('teste');
+  }
+
   return (
     <main>
       <h1>Home</h1>
+      <button onClick={handleClick}>Definir Cookie</button>
     </main>
   );
 }
