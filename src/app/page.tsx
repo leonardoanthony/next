a15d79fce294ca1e4f3d58db17114f4796833bfd
leonardoanthony <leonardoanthony.dev@gmail.com>
2025-03-18 22:45:11 -0300

    :sparkles: feat: use Params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-'use client'
-
-
 export default function HomePage() {
-
-
   return (
     <main>
       <h1>Home</h1>
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 
-export default function Menu() {
+export default async function Menu() {
   return (
     <ul className="menu">
       <li>
         <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/acoes">Ações</Link>
       </li>
     </ul>
   );
