aaa1d4d8790f52434e43bdad5afe2dc40f884fa4
leonardoanthony <leonardoanthony.dev@gmail.com>
2025-03-19 16:08:55 -0300

    :sparkles: feat: useRouter e useSearchParams

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,26 +1,42 @@
 "use client";
 
 import Link from 'next/link';
-import { useParams, usePathname } from 'next/navigation';
-import { useEffect } from 'react';
+import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { Suspense, useEffect } from 'react';
+
+function Busca(){
+  const searchParams = useSearchParams();
+  const busca = searchParams.get('busca');
+  return <div>Busca: {busca}</div>
+}
 
 export default function Menu() {
   const params = useParams();
   const pathName = usePathname();
 
+  const router = useRouter();
+
   useEffect(() => {
-    console.log('pathName', pathName)
+    console.log('pathName', pathName);
+    // setTimeout(() => {
+    //   router.refresh();
+    // }, 5000)
   }, [pathName])
 
   console.log(pathName)
   return (
+    <>
+    <Suspense fallback={'Carregando...'}>
+      <Busca />
+    </Suspense>
     <ul className="menu">
       <li>
         <Link href="/">Home</Link>
       </li>
       <li>
-        <Link href="/acoes">Ações</Link>
+        <Link href="/acoes/?busca=xpt">Ações</Link>
       </li>
     </ul>
+    </>
   );
 }
